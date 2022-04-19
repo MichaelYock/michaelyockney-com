d36aafaaf0ee6b5109c83a20ec000818ba3a6c8e
Michael Y <michael.yockney@gmail.com>
2022-04-19 18:19:09 +1200

    add email in footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}
+        <p>© {new Date().getFullYear()}<br />hi@michaelyockney.com</p>
       </footer>
     </div>
   )
